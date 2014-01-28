c846653ae46f45845972eb5538fba71756962165
John Heerman <jheerman@gmail.com>
2014-01-28 11:29:27 -0600

    Create event handler for alternating possession arrow Add title label

--- src/jumpballViewController.cs
+++ src/jumpballViewController.cs
@@ -56,18 +56,58 @@ namespace jumpball
 			// Release any cached data, images, etc that aren't in use.
 		}
 
+		void alternatePossession (object sender, EventArgs args)
+		{
+			possessionHistory.Add (_position);
+			if (_position == Arrow.Left) {
+				//set the animation left to right
+				_arrow.AnimationImages = left_to_right_arrows;
+				_arrow.AnimationRepeatCount = 1;
+				_arrow.AnimationDuration = .5;
+				UIView.BeginAnimations ("rotateAnimation");
+				UIView.SetAnimationDelegate (this);
+				UIView.SetAnimationDidStopSelector (new Selector ("rotateAnimationFinished:"));
+				UIView.CommitAnimations ();
+				_arrow.StartAnimating ();
+			}
+			else {
+				//set the animation right to left
+				_arrow.AnimationImages = right_to_left_arrows;
+				_arrow.AnimationRepeatCount = 1;
+				_arrow.AnimationDuration = .5;
+				UIView.BeginAnimations ("rotateAnimation");
+				UIView.SetAnimationDelegate (this);
+				UIView.SetAnimationDidStopSelector (new Selector ("rotateAnimationFinished:"));
+				UIView.CommitAnimations ();
+				_arrow.StartAnimating ();
+			}
+		}
+
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
+
 			SEssentialsSlidingOverlay slidingView = new SEssentialsSlidingOverlay (View.Frame, true);
 			slidingView.Overlay.BackgroundColor = UIColor.White;
 			slidingView.UnderlayRevealAmount = UserInterfaceIdiomIsPhone ? .66f : .33f;
 			slidingView.Underlay.AddShadowTop ();
+			slidingView.Toolbar.BackgroundColor = UIColor.Red;
 			slidingView.Overlay.AddSubview (_arrow);
+
+			var title = new UILabel () { 
+				Text = "Jumpball",
+				TextAlignment = UITextAlignment.Center,
+				Frame = new RectangleF(0,0,View.Frame.Width, 50),
+				Font = UIFont.BoldSystemFontOfSize(20),
+				TextColor = UIColor.Red
+			};
+
+			slidingView.Overlay.AddSubview (title);
 
 			_arrow.Center = View.Center;
 			_arrow.AutosizesSubviews = true;
 			_arrow.AutoresizingMask = UIViewAutoresizing.FlexibleMargins;
+
 
 			_alternate = new UIButton (new RectangleF (0, View.Frame.Bottom - 100, View.Frame.Width, 55));
 			_alternate.AutosizesSubviews = true;
@@ -75,44 +115,16 @@ namespace jumpball
 			_alternate.SetTitleColor (UIColor.Blue, UIControlState.Normal);
 			_alternate.HorizontalAlignment = UIControlContentHorizontalAlignment.Center;
 			_alternate.SetTitle ("Alternate", UIControlState.Normal);
-			_alternate.TouchUpInside += (object sender, EventArgs e) => {
-				possessionHistory.Add(_position);
-				if (_position == Arrow.Left)
-				{
-					//set the animation left to right
-					_arrow.AnimationImages = left_to_right_arrows;
-					_arrow.AnimationRepeatCount = 1;
-					_arrow.AnimationDuration = .5;
-
-					UIView.BeginAnimations("rotateAnimation");
-					UIView.SetAnimationDelegate (this);
-					UIView.SetAnimationDidStopSelector (
-						new Selector ("rotateAnimationFinished:"));
-					UIView.CommitAnimations();
-
-					_arrow.StartAnimating();
-				}
-				else
-				{
-					//set the animation right to left
-					_arrow.AnimationImages = right_to_left_arrows;
-					_arrow.AnimationRepeatCount = 1;
-					_arrow.AnimationDuration = .5;
-
-					UIView.BeginAnimations("rotateAnimation");
-					UIView.SetAnimationDelegate (this);
-					UIView.SetAnimationDidStopSelector (
-						new Selector ("rotateAnimationFinished:"));
-					UIView.CommitAnimations();
-
-					_arrow.StartAnimating();
-
-				}
-			};
+			_alternate.TouchUpInside += alternatePossession;
 
 			slidingView.Overlay.AddSubview (_alternate);
-			slidingView.Style.ButtonTintColor = UIColor.Black;
+			slidingView.Style.ButtonTintColor = UIColor.White;
 			View.AddSubview (slidingView);
+		}
+
+		public override bool PrefersStatusBarHidden ()
+		{
+			return true;
 		}
 
 		[Export("rotateAnimationFinished:")]
